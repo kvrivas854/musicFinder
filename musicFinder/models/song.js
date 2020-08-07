c3f1a4e4aa6386b9bf098162a44fa0599a93a56c
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-08-07 18:16:10 -0400

    uploaded models

--- musicFinder/models/song.js
+++ musicFinder/models/song.js
@@ -1,18 +1,32 @@
-// module.exports = function(sequelize, DataTypes) {
-//     var Song = sequelize.define("Song", {
-//       // Giving the Author model a name of type STRING
-//       name: DataTypes.STRING
-//     });
+module.exports = function(sequelize, DataTypes) {
+    var Song = sequelize.define("Song", {
+      // Giving the Author model a name of type STRING
+      name:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[1,100]
+        }
+        },
+        added:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
   
-//     Song.associate = function(models) {
-//       // Associating Author with Posts
-//       // When an Author is deleted, also delete any associated Posts
-//         Song.hasOne(models.Artist.id)
-//     };
+    Song.associate = function(models) {
+        Song.belongsTo(models.Artist, {through: "ArtistSong"})
+    };
+    Song.associate = function(models) {
+        Song.belongsToMany(models.Playlist, { through: "SongPlaylist"});
+    }
+    Song.associate = function(models) {
+          Song.belongsTo(models.Album, {through: "AlbumSong"})
+      };
     
     
   
-//     return Song;
-// };
+    return Song;
+};
 
   
